adab28dbf514cc85340dc16654f8e757e0c18dd8
rimaakter1 <rimac201262@gmail.com>
2024-12-06 19:17:20 +0600

    added highest rated game section

---
+++ src/components/HighestRatedGame/HighestRatedGame.jsx
@@ -0,0 +1,17 @@
+import ReviewsCard from "../ReviewsCard/ReviewsCard";
+
+const HighestRatedGame = ({ topRatedReviews }) => {
+
+    return (
+        <div>
+            <h1>Highest Rated Game</h1>
+            <div className="grid grid-cols-3 gap-5">
+                {
+                    topRatedReviews.map(review => <ReviewsCard key={review._id} review={review}></ReviewsCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default HighestRatedGame;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import { useLoaderData } from "react-router";
 import Banner from "../components/Banner/Banner";
+import HighestRatedGame from "../components/HighestRatedGame/HighestRatedGame";
 
 const Home = () => {
+    const topRatedReviews = useLoaderData();
+    console.log(topRatedReviews);
     return (
         <div>
             <Banner></Banner>
+            <HighestRatedGame topRatedReviews={topRatedReviews}></HighestRatedGame>
         </div>
     );
 };
