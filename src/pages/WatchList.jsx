e1e230c1e50aad8a404336dcd8354e54baf8e4ca
rimaakter1 <rimac201262@gmail.com>
2024-12-06 22:57:02 +0600

    added my watchlist functionalities

--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -1,8 +1,26 @@
+import { useContext, useState } from "react";
+import { useEffect } from "react";
+import { authContext } from "../components/AuthProvider/AuthProvider";
+import ReviewsCard from "../components/ReviewsCard/ReviewsCard";
 
 const WatchList = () => {
+    const { user } = useContext(authContext);
+    const [watchlistData, setWatchlistData] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/watchlist?userEmail=${user.email}`)
+            .then(res => res.json())
+            .then(data => setWatchlistData(data))
+    }, [])
+
+
     return (
         <div>
             <h1>WatchList</h1>
+            {
+
+                watchlistData.map(review => <ReviewsCard key={review._id} review={review}></ReviewsCard>)
+            }
         </div>
     );
 };
